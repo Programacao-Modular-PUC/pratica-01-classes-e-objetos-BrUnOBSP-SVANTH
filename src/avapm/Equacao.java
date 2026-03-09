d7fbe91efc988bf99ad3a603ead5058529257b31
Bruno da Silva de Paulo <1593111@sga.pucminas.br>
2026-03-09 11:50:48 -0300

    primeiro commit

---
+++ src/avapm/Equacao.java
@@ -0,0 +1,113 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package avapm;
+/**
+ *
+ * @author 1593111
+ */
+public class Equacao {
+    private double a;
+    private double b;
+    private double c;
+    private double del;
+    private double x1;
+    private double x2;
+    
+           
+
+public Equacao(){
+    this.a = 0.0;
+    this.b = 0.0;
+    this.c = 0.0;
+    this.del = 0.0;
+    this.x1 = 0.0;
+    this.x2 = 0.0;
+}
+
+    /**
+     * @return the a
+     */
+    public double getA() {
+        return a;
+    }
+
+    /**
+     * @param a the a to set
+     */
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    /**
+     * @return the b
+     */
+    public double getB() {
+        return b;
+    }
+
+    /**
+     * @param b the b to set
+     */
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    /**
+     * @return the c
+     */
+    public double getC() {
+        return c;
+    }
+
+    /**
+     * @param c the c to set
+     */
+    public void setC(double c) {
+        this.c = c;
+    }
+
+    /**
+     * @return the del
+     */
+    public double getDel() {
+        return del;
+    }
+
+    /**
+     * @param del the del to set
+     */
+    public void setDel(double del) {
+        this.del = del;
+    }
+
+    /**
+     * @return the x1
+     */
+    public double getX1() {
+        return x1;
+    }
+
+    /**
+     * @param x1 the x1 to set
+     */
+    public void setX1(double x1) {
+        this.x1 = x1;
+    }
+
+    /**
+     * @return the x2
+     */
+    public double getX2() {
+        return x2;
+    }
+
+    /**
+     * @param x2 the x2 to set
+     */
+    public void setX2(double x2) {
+        this.x2 = x2;
+    }
+
+}       
